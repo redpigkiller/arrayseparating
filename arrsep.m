5274116ea47e73961d171cded0a0dee4f3d7da03
redpigkiller <113985496+redpigkiller@users.noreply.github.com>
2023-05-03 01:05:20 +0800

    update for the fix on thres

--- arrsep.m
+++ arrsep.m
@@ -1,4 +1,4 @@
-function new_arr = arrsep(arr, gap, cirbnd, thres)
+function new_arr = arrsep(arr, gap, cirbnd, tot)
 %ARRSEP This function seperates the array for every "circular" distance 
 % between any two points are greater than gap. Make sure that the input 
 % range is [-cirbnd, cirbnd]. If not, the distance between two points may 
@@ -8,21 +8,21 @@ function new_arr = arrsep(arr, gap, cirbnd, thres)
 %     Usage:
 %         out = arrsep(in, gap)
 %         out = arrsep(in, gap, cirbnd)
-%         out = arrsep(in, gap, cirbnd, eps)
+%         out = arrsep(in, gap, cirbnd, tot)
 % 
 %         [input]:
 %             in:     the input array
 %             gap:    the minimum distance between adjacent point in the input array
 %             cirbnd: the circular bound for the array, this is used to measure the "distance" between angles, e.g. the "distance" between -0.49 and 0.49
 %                     is 0.1 + 0.1 = 0.2 with cirbnd = 0.5
-%             eps:    the small value to perform the comparsion between two float/bouble variables
+%             tot:    the small value to perform the comparsion between two float/bouble variables
 %         
 %         [output]:
 %             out:    the output array
 % 
 %         [default values]:
 %             cirbnd: inf
-%             eps:    1e-9
+%             tot:    1e-16
 %   ref:
 %       https://www.mathworks.com/help/matlab/matlab_external/c-mex-source-file.html
 %       https://www.mathworks.com/help/matlab/matlab_external/cpp-mex-api.html
